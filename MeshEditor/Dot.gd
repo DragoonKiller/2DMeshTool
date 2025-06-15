348a3fd2789f1ddafb0acfe65bb1cc5ee601afee
Narellanda Karatoga <karatoga@qq.com>
2025-06-15 11:05:00 +0800

    保存加载图片的路径

--- MeshEditor/Dot.gd
+++ MeshEditor/Dot.gd
@@ -16,9 +16,9 @@ func _process(_delta:float) -> void:
 func _draw() -> void:
 	
 	if _selected:
-		draw_circle(Vector2.ZERO, 5, Color.CORNFLOWER_BLUE, true)
+		draw_circle(Vector2.ZERO, 5, Color.ANTIQUE_WHITE, true)
 		draw_string(ResIndex.get_default_font(), Vector2.UP * 10, self.name)
 	else:
-		draw_circle(Vector2.ZERO, 5, Color.ANTIQUE_WHITE, true)
+		draw_circle(Vector2.ZERO, 5, Color.CORNFLOWER_BLUE, true)
 		
 	
--- MeshEditor/SpriteDisplay.gd
+++ MeshEditor/SpriteDisplay.gd
@@ -6,12 +6,30 @@ var dialog :FileDialog
 @export
 var image :Image
 
+@export
+var _previous_path :String
+var previous_path:
+	get:
+		if _previous_path:
+			return _previous_path
+		var file = FileAccess.open("user://previous_path.txt", FileAccess.READ)
+		if file == null:
+			return ""
+		var res = file.get_line()
+		file.close()
+		return res
+	set(value):
+		var file = FileAccess.open("user://previous_path.txt", FileAccess.WRITE)
+		file.store_string(value)
+		file.close()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("OpenFile"):
 		if not dialog:
 			dialog = FileDialog.new()
 		add_child(dialog)
 		dialog.connect("file_selected", on_file_selected)
+		dialog.current_dir = previous_path
 		dialog.access = FileDialog.ACCESS_FILESYSTEM
 		dialog.use_native_dialog = true
 		dialog.file_mode = FileDialog.FILE_MODE_OPEN_ANY
@@ -23,6 +41,7 @@ func _input(event: InputEvent) -> void:
 
 func on_file_selected(path: String):
 	print('select file ', path)
+	previous_path = path.get_base_dir()
 	
 	image = Image.load_from_file(path)
 	
